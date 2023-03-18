c574db16058597d850be783a826dab642501922d
dipendra pandey <pandeydipendra678@gmail.com>
2023-03-18 12:20:04 +0530

    nobar

--- app/src/main/java/com/example/bmicalc_muinew/more_about_BMI.kt
+++ app/src/main/java/com/example/bmicalc_muinew/more_about_BMI.kt
@@ -1,13 +1,21 @@
 package com.example.bmicalc_muinew
 
 import android.app.ProgressDialog
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.core.view.isEmpty
 import com.example.bmicalc_muinew.databinding.ActivityMoreAboutBmiBinding
+import com.google.android.material.snackbar.Snackbar
+
+private fun Snackbar.setAction(s: String) {
+
+}
 
 class more_about_BMI : AppCompatActivity() {
     private lateinit var binding: ActivityMoreAboutBmiBinding
@@ -20,24 +28,26 @@ class more_about_BMI : AppCompatActivity() {
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.item3 -> {
-                ProgressDialog(this).apply {
-                    this.setCancelable(false)
-                    this.setTitle(R.string.app_name)
-                    this.setMessage("please Wait..")
-                    this.setProgressStyle(ProgressDialog.STYLE_SPINNER)
-                }
-                    .show()
-            }
 
 
-        }
-
-
-        return true
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.item3 -> {
+//                ProgressDialog(this).apply {
+//                    this.setCancelable(false)
+//                    this.setTitle(R.string.app_name)
+//                    this.setMessage("please Wait..")
+//                    this.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+//                }
+//                    .show()
+//            }
+//
+//
+//        }
+//
+//
+//        return true
+//    }
 
     inner class MyWebViewClient : WebViewClient() {
         override fun onPageFinished(view: WebView?, url: String?) {
@@ -45,6 +55,24 @@ class more_about_BMI : AppCompatActivity() {
             binding.progressbar.visibility = View.GONE
         }
 
+
     }
+//    override fun onOptionsItemSelected(item: MenuItem) {
+//        when (item.itemId) {
+//            R.id.item3 -> {
+//                if (binding.webview.isEmpty()) {
+//                    Snackbar.make(binding.root, "Network Error", Snackbar.LENGTH_SHORT)
+//                        .setAction("Retry", object : View.OnClickListener {
+//                            override fun onClick(v: View?) {
+//                                Toast.makeText(this, "loading", Toast.LENGTH_SHORT).show()
+//                            }
+//                        })
+//                        .show()
+//                }
+//            }
+//
+//
+//        }
+//    }
 
 }
